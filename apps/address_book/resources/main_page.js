22af4560b439df7172a7bd64b0fa3bf3f9f7c8d2
Jeff Pittman <geojeff@me.com>
2012-02-23 03:53:32 -0600

    Added workspace view, replacing the Hello World label. So now have a toolbar at the top and a splitview as the main body.

--- apps/address_book/resources/main_page.js
+++ apps/address_book/resources/main_page.js
@@ -11,14 +11,14 @@ AddressBook.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
-    
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
+    childViews: 'workspaceView'.w(),
+    workspaceView: SC.WorkspaceView.design({
+      contentView: SC.SplitView.design({
+        dividerThickness: 1,
+        defaultThickness: 300,
+        topLeftView: SC.View,
+        bottomRightView: SC.View
+      })
     })
   })
-
 });
